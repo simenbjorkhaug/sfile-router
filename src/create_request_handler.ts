2a6c01410d51b680509df6aa2a0b7fa14488799b
Simen Bjorkhaug <simen@bjorkhaug.io>
2024-02-06 23:11:19 +0100

    fix: route with param route on same route does overwrite a route

--- src/create_request_handler.ts
+++ src/create_request_handler.ts
@@ -3,50 +3,62 @@ import { Config, routes } from "./routes.ts";
 import { extractParamsAndMatch } from "./extract_params_and_match.ts";
 
 export type Options = {
-  shouldThrow?: boolean
-}
+  shouldThrow?: boolean;
+};
 
 export class NotFoundError extends Error {}
 
 export class MethodNotAllowedError extends Error {}
 
 // deno-lint-ignore no-explicit-any
-export const createRequestHandler = function<T extends typeof routes, C = any>(_routes: T, request: Request, context?: C) {
-    const url = new URL(request.url)
-  
-    const path = url.pathname
-  
-    for (const [route, methods] of _routes.entries()) {
-      const params = extractParamsAndMatch(path, route)
-      
-      if (params) {
-        const method = request.method.toUpperCase() as Methods
-  
-        if (methods.has(method)) {
-          const { handler } = methods.get(method)!
-  
-          return handler({ request, params, context: context ?? {} })
-        }
-  
-        if (Config.shouldThrow) {
-          throw new MethodNotAllowedError(`Method ${method} not allowed`)
-        }
+export const createRequestHandler = function <T extends typeof routes, C = any>(
+  _routes: T,
+  request: Request,
+  context?: C,
+) {
+  const url = new URL(request.url);
 
-        return new Response('Method not allowed', { status: 405 })
+  const path = url.pathname;
+
+  for (const [route, methods] of _routes) {
+    const params = extractParamsAndMatch(path, route);
+
+    if (params) {
+      const method = request.method.toUpperCase() as Methods;
+
+      if (methods.has(method)) {
+        const { handler } = methods.get(method)!;
+
+        return handler({ request, params, context: context ?? {} });
       }
-    }
-  
-    if (Config.shouldThrow) {
-      throw new NotFoundError('Not found')
-    }
 
-    return new Response('Not found', { status: 404 })
+      if (Config.shouldThrow) {
+        throw new MethodNotAllowedError(`Method ${method} not allowed`);
+      }
+
+      return new Response("Method not allowed", { status: 405 });
+    }
+  }
+
+  if (Config.shouldThrow) {
+    throw new NotFoundError("Not found");
+  }
+
+  return new Response("Not found", { status: 404 });
   // deno-lint-ignore no-explicit-any
-  }.bind(null, routes) as <T = any>(request: Request, context?: T) => Promise<Response> | Response
+}.bind(null, routes) as <T = any>(
+  request: Request,
+  context?: T,
+) => Promise<Response> | Response;
 
 // deno-lint-ignore no-explicit-any
-export type RequestHandlerArgs<T = Map<string, string>, C = any> = { request: Request, params: T, context: C }
+export type RequestHandlerArgs<T = Map<string, string>, C = any> = {
+  request: Request;
+  params: T;
+  context: C;
+};
 
 // deno-lint-ignore no-explicit-any
-export type RequestHandler<T, C = any> = (arg: RequestHandlerArgs<T, C>) => Promise<Response> | Response
-  
+export type RequestHandler<T, C = any> = (
+  arg: RequestHandlerArgs<T, C>,
+) => Promise<Response> | Response;
--- src/routes.ts
+++ src/routes.ts
@@ -1,60 +1,102 @@
-import { expandGlob } from "https://deno.land/std@0.213.0/fs/mod.ts"
-import { existsSync } from 'https://deno.land/std@0.213.0/fs/exists.ts'
-import { methods } from "./methods.ts"
+import { expandGlob } from "https://deno.land/std@0.213.0/fs/mod.ts";
+import { existsSync } from "https://deno.land/std@0.213.0/fs/exists.ts";
+import { methods, type Methods } from "./methods.ts";
 
-export const routes = new Map<string, Map<typeof methods[number], {
-    // deno-lint-ignore no-explicit-any
-    handler: ({ params, request, context }: { params: Map<string, null | string>, request: Request, context: any }) => Promise<Response> | Response
-}>>()
+type Handler = ({
+  params,
+  request,
+  context,
+}: {
+  params: Map<string, null | string>;
+  request: Request;
+  context: unknown;
+}) => Promise<Response> | Response;
+
+/** I need to sort routes so that /health is before /:name etc, so that specified paths are matched against first */
+export const routes: [string, Map<Methods, { handler: Handler }>][] = [];
 
 export const Config = {
-  shouldThrow: false
-}
+  shouldThrow: false,
+};
 
-export async function configureRoutes({ directory = './routes', config }: { directory?: string , config?: typeof Config } = {}) {
+export async function configureRoutes({
+  directory = "./routes",
+  config,
+}: { directory?: string; config?: typeof Config } = {}) {
+  const _routes = new Map<
+    string,
+    Map<
+      (typeof methods)[number],
+      {
+        handler: ({
+          params,
+          request,
+          context,
+        }: {
+          params: Map<string, null | string>;
+          request: Request;
+          context: unknown;
+        }) => Promise<Response> | Response;
+      }
+    >
+  >();
+
   if (!directory) {
-    directory = './routes'
+    directory = "./routes";
   }
-  
-  if (!existsSync(directory, {
-    isDirectory: true,
-    isReadable: true
-  })) {
-    throw new Error('No routes directory found, please create one.')
+
+  if (
+    !existsSync(directory, {
+      isDirectory: true,
+      isReadable: true,
+    })
+  ) {
+    throw new Error("No routes directory found, please create one.");
   }
 
   if (config) {
-    Object.assign(Config, config)
+    Object.assign(Config, config);
   }
 
   for await (const file of expandGlob(`${directory}/*.ts`)) {
-    const fileContents = await import(file.path)
+    const fileContents = await import(file.path);
 
-    const [,file_path] = file.path.split('routes')
+    const [, file_path] = file.path.split("routes");
 
     let router_path = `/${file_path
-      .replace(/^\//, '') // replacing leading slash from file_path
-      .replace(/^_[^\.]*\./, '') // allow for grouping routes
-      .replace(/^\./, '') // remove leading dot
-      .replace(/\(\.\)/g, 'ESCAPED_DOT') // temporarily replace dots in parentheses
-      .replace('.ts', '') // replace extension
-      .replace(/\./g , '/') // replace dots with slashes
-      .replace(/ESCAPED_DOT/g, '.') // put dots in parentheses back in
-      .replace(/\[(.*?)\]/g, ':$1')}` // replace square brackets with colons
+      .replace(/^\//, "") // replacing leading slash from file_path
+      .replace(/^_[^\.]*\./, "") // allow for grouping routes
+      .replace(/^\./, "") // remove leading dot
+      .replace(/\(\.\)/g, "ESCAPED_DOT") // temporarily replace dots in parentheses
+      .replace(".ts", "") // replace extension
+      .replace(/\./g, "/") // replace dots with slashes
+      .replace(/ESCAPED_DOT/g, ".") // put dots in parentheses back in
+      .replace(/\[(.*?)\]/g, ":$1")}`; // replace square brackets with colons
 
-
-    if (router_path === '/index') {
-      router_path = '/'
+    if (router_path === "/index") {
+      router_path = "/";
     }
 
-    routes.set(router_path, new Map())
+    _routes.set(router_path, new Map());
 
     for (const method of methods) {
       if (fileContents[method]) {
-        routes.get(router_path)?.set(method, {
-          handler: fileContents[method]
-        })
+        _routes.get(router_path)?.set(method, {
+          handler: fileContents[method],
+        });
       }
     }
+
+    routes.push(
+      ...Array.from(_routes.entries()).toSorted((a, b) => {
+        if (a[0].includes(":") && !b[0].includes(":")) {
+          return 1;
+        } else if (!a[0].includes(":") && b[0].includes(":")) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }),
+    );
   }
 }
